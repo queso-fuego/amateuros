30a7c2dea506e810f4f30144053d96260ff7e669
Fire Cheese <fuegoqueso@gmail.com>
2020-06-26 11:36:01 -0500

    Added new save_file & load_file include files for saving & loading sectors to/from disk

--- src/editor.asm
+++ src/editor.asm
@@ -18,11 +18,15 @@ reset_editor:
 	mov es, ax			
 	mov word di, 0F00h	; ES:DI <- 0B8000h:80*2*24
 	
-	mov si, keybinds	
-	mov cx, 33			; # of bytes to move
+	mov si, keybinds_string	
+	mov cx, 56			; # of bytes to move
 	cld					; clear direction flag (increment operands)
 
-	rep movsw			; mov [di], [si] and increment both until cx = 0
+	mov al, byte [text_color]
+	.loop:
+		movsb			; mov [di], [si] and increment both; store character
+		stosb			; Store character attribute byte (text colors)
+	loop .loop
 
 	;; Restore extra segment
 	mov ax, 800h
@@ -52,7 +56,7 @@ return_from_hex: jmp get_next_hex_char
 	;; When done with input, convert to valid machine code (hex) & run	
 execute_input:
 	mov byte [di], 0C3h ; C3 hex = near return x86 instruction, to get back to prompt after running code
-	jmp hex_code		; jump to hex code memory location to run
+	call hex_code		; jump to hex code memory location to run
 
 	jmp reset_editor	; reset for next hex input
 	
@@ -85,20 +89,27 @@ end_editor:
         mov es, ax
         mov fs, ax
         mov gs, ax
-        jmp 200h:0000h				; far jump back to kernel
+        jmp 200h:0000h			; far jump back to kernel
 
 	;; include files
 	include "../include/screen/clear_screen_text_mode.inc"
 	include "../include/print/print_string.inc"
+	include "../include/disk/load_file.inc"
+	include "../include/disk/save_file.inc"
 	
 	;; VARIABLES
 	;; --------------------------
 testString:	db 'Testing',0
-keybinds:	db '$',17h,' ',17h,'=',17h,' ',17h,'R',17h,'u',17h,'n',17h,' ',17h,'c',17h,'o',17h,'d',17h,'e',17h,\
-' ',17h,'?',17h,' ',17h,'=',17h,' ',17h,'R',17h,'e',17h,'t',17h,'u',17h,'r',17h,'n',17h,' ',17h,\
-'t',17h,'o',17h,' ',17h,'k',17h,'e',17h,'r',17h,'n',17h,'e',17h,'l',17h
+keybinds_string:	db ' $ = Run code ? = Return to kernel S = Save file to disk'
+;keybinds_string_len equ $ - keybinds_string
+new_or_current_string: db '(C)reate new file or (L)oad current file?'
+;; 41 length
+choose_filetype_string: db '(B)inary/hex file or (O)ther file type (.txt, etc)?'
+;; 51 length
+
 hex_byte:	db 00h		; 1 byte/2 hex digits
 hex_code:	times 255 db 00h
+text_color: db 17h
 
 	;; Sector padding
-	times 1024-($-$$) db 0
+	times 1536-($-$$) db 0
--- test/test.asm
+++ test/test.asm
@@ -5,3 +5,5 @@
 	mov ah, 0x0e
 	mov al, 'T'
 	int 0x10
+	cli
+	hlt
